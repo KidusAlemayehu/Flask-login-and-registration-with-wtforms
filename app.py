ff333e26f04a87a7f5352918ab947dbf58522560
KidusAlemayehu <kidusalemayehu705@gmail.com>
2021-12-03 14:28:21 +0300

    On branch main

--- app.py
+++ app.py
@@ -4,18 +4,21 @@ from forms import RegistrationForm, LoginForm
 from model import User
 from flask_migrate import Migrate
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import login_user
+from login import login_manager
 
 app = Flask(__name__)
 db.init_app(app)
+login_manager.init_app(app)
 migrate = Migrate(app, db)
 app.config['SECRET_KEY'] = 'e581349e00c89fc8ffa0ae9f8e04828c8bc8b016359c99860255a11965134317'
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:'your_db_pwd'@localhost/Flask-Login-with-WTForms"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://postgres:3346khag@localhost/Flask-Login-with-WTForms"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
 @app.route("/")
-def index(username):
-    return render_template('index.html', username=username, title='Home')
+def index():
+    return render_template('index.html', title='Home')
 
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -26,16 +29,13 @@ def login():
             email = form.email.data
             password = form.password.data
             user = User.get_user_by_email(email=email)
-            if user:
-                if not check_password_hash(user.password, password):
-                    flash(f'incorrect password', 'danger')
-                    return redirect(url_for('login'))
-                else:
-                    return redirect(url_for('index', username=user.username))
-            else:
+            if user and check_password_hash(user.password, password):
+                login_user(user)
+                return redirect(url_for('index'))
+            elif not user:
                 flash(f'there is no account with this Email Address !', 'danger')
-                return redirect(url_for('login'))
-    if request.method == 'GET':
+                return redirect(url_for('register'))
+    elif request.method == 'GET':
         return render_template('login.html', form=form, title='Login')
 
 
@@ -58,7 +58,8 @@ def register():
             elif User.get_user_by_email(email=email):
                 flash(f'Email is Already in use', 'danger')
                 return redirect(url_for('register'))
-            hs_pwd = generate_password_hash(org_pwd, method='pbkdf2:sha256')
+            hs_pwd = generate_password_hash(
+                org_pwd, method='pbkdf2:sha256', salt_length=16)
             user = User(username=username, email=email, password=hs_pwd)
             user.save()
             flash(
--- model.py
+++ model.py
@@ -1,4 +1,10 @@
 from db import db
+from login import login_manager
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 class User(db.Model):
